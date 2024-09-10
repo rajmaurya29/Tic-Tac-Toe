0b38c64747bc44ff62f3fe32b4478bbbddb7c8d0
rajmaurya29 <rajmaurya29092004@gmail.com>
2024-09-10 23:19:32 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Square from './Square'
+let t1=[null,null,null,null,null,null,null,null,null];
 function App() {
   const [xIsNext,setxIsNext]=useState(true);
   const [squares,setSquares]=useState(Array(9).fill(null));
   let win;
+  
   const winner=calculatewinner(squares);
     if(winner=="X"||winner=="O"){
       win ="Congratulations! Player "+winner+" Wins";
     }
     else if(winner=="1"){
-      win ="TIED";
+      win ="Draw";
     }
     else {
       win="Turn :    Player   "+(xIsNext?"X":"O");
@@ -39,7 +41,7 @@ function App() {
   return (
     <div className="maincontainer">
     <div className='container'>
-      <h1 className='mainheading'>TIC TAC TOE</h1>
+      <div className='headingtic'><h1 className='mainheading'>TIC TAC TOE</h1></div>
       
       <div className="container3">
           <button className='newgamebutton' onClick={handleClick1}>New Game</button>
@@ -49,19 +51,19 @@ function App() {
         <div className="container1">
         
           <div className='board-row'>
-            <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-            <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
-            <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+            <Square value={squares[0]} onSquareClick={()=>handleClick(0)} col={t1[0]}/>
+            <Square value={squares[1]} onSquareClick={()=>handleClick(1)} col={t1[1]}/>
+            <Square value={squares[2]} onSquareClick={()=>handleClick(2)} col={t1[2]}/>
           </div>
             <div className='board-row'>
-              <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
-              <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
-              <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+              <Square value={squares[3]} onSquareClick={()=>handleClick(3)} col={t1[3]}/>
+              <Square value={squares[4]} onSquareClick={()=>handleClick(4)} col={t1[4]}/>
+              <Square value={squares[5]} onSquareClick={()=>handleClick(5)} col={t1[5]}/>
             </div>
           <div className='board-row'>
-            <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
-            <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
-            <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+            <Square value={squares[6]} onSquareClick={()=>handleClick(6)} col={t1[6]}/>
+            <Square value={squares[7]} onSquareClick={()=>handleClick(7)} col={t1[7]}/>
+            <Square value={squares[8]} onSquareClick={()=>handleClick(8)} col={t1[8]}/>
           </div>
         </div>
 
@@ -78,10 +80,18 @@ function calculatewinner(squares){
     for(var i=0;i<t.length;i++){
       const [a,b,c]=t[i];
       if(squares[a]&& squares[a]===squares[b] && squares[a]===squares[c]){
+        t1[a]=1;t1[b]=1;t1[c]=1;
+        console.log(t1[0]+", "+t1[1]+", "+t1[2]+", "+t1[3]+", "+t1[4]+", "+t1[5]+", "+t1[6]+", "+t1[7]+", "+t1[8]);
+        // console.log(a+" "+b+" "+c+" a");
         return squares[a];
       }
-      if(squares[0]&&squares[1]&&squares[1]&&squares[2]&&squares[3]&&squares[4]&&squares[5]&&squares[6]&&squares[7]&&squares[8]){
+    }
+    for(var j=0;j<t.length;j++){
+      const [a,b,c]=t[j];
+        if((squares[0]&&squares[1]&&squares[1]&&squares[2]&&squares[3]&&squares[4]&&squares[5]&&squares[6]&&squares[7]&&squares[8])){
+        // console.log(a+" "+b+" "+c+" b");
         return "1";
+
       }
     }
 
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 function Square(props){
     let a=props.value;
+    let clr=props.col;
+    console.log(a+" "+clr);
     let b="#D0F0C0";
+    let k="#D0F0C0";
     if(a!=null){
+        if(props.value=="X")
         b="black";
+        else{
+        b="white";
+        }
     }
+    if(clr==null){
+        console.log(4);
+    }
+    if(clr==1){
+        k="black";
+        b="white";
+        console.log(1);
+    }
+
     return(
-        <button type="button" className='square' style={{color:b}} onClick={props.onSquareClick}>{props.value?props.value:"_"}</button>
+        <button type="button" className='square' style={{color:b,backgroundColor:k}} onClick={props.onSquareClick}>{props.value?props.value:"_"}</button>
  )
 }
 export default Square;
